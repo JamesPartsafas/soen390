19cf3f8208fc18521c5daac0b2c64e982f33b0d6
Chad Buck <chadbuck13@gmail.com~>
2023-01-23 12:57:19 -0500

    Styled navbar, footer, home and login page

--- synapsis/src/main/java/com/soen/synapsis/index/IndexController.java
+++ synapsis/src/main/java/com/soen/synapsis/index/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "")
+@RequestMapping(path = "/")
 public class IndexController {
 
     private final IndexService indexService;
