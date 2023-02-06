5499f5ceb6d4ce3078f4860de7061084ac8d4ba8
Kenny Phan <kennyphan100@gmail.com>
2023-02-05 23:23:22 -0500

    #24 Update unit tests to ensure that the connection is saved to the database, and write the unit test for when an error is thrown for an already existing connection

--- synapsis/src/test/java/com/soen/synapsis/unit/appuser/connection/ConnectionServiceTest.java
+++ synapsis/src/test/java/com/soen/synapsis/unit/appuser/connection/ConnectionServiceTest.java
@@ -2,31 +2,46 @@ package com.soen.synapsis.unit.appuser.connection;
 
 import com.soen.synapsis.appuser.AppUser;
 import com.soen.synapsis.appuser.Role;
+import com.soen.synapsis.appuser.connection.Connection;
+import com.soen.synapsis.appuser.connection.ConnectionKey;
 import com.soen.synapsis.appuser.connection.ConnectionRepository;
 import com.soen.synapsis.appuser.connection.ConnectionService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 
 public class ConnectionServiceTest {
 
-    private ConnectionService connectionService;
-
+    private ConnectionService underTest;
     @Mock
     private ConnectionRepository connectionRepository;
-
     private AutoCloseable autoCloseable;
+    private AppUser candidateUser1;
+    private AppUser candidateUser2;
+    private AppUser companyUser;
+    private AppUser recruiterUser;
+    private AppUser adminUser;
 
     @BeforeEach
     void setUp() {
         autoCloseable = MockitoAnnotations.openMocks(this);
-        connectionService = new ConnectionService(connectionRepository);
+        underTest = new ConnectionService(connectionRepository);
+
+        candidateUser1 = new AppUser(1L, "Joe Man", "1234", "joeman@email.com", Role.CANDIDATE);
+        candidateUser2 = new AppUser(2L,"Joe Woman", "1234", "joewoman@email.com", Role.CANDIDATE);
+        companyUser = new AppUser(3L,"Company", "1234", "company@email.com", Role.COMPANY);
+        recruiterUser = new AppUser(4L,"Recruiter", "1234", "recruiter@email.com", Role.RECRUITER);
+        adminUser = new AppUser(5L,"Admin", "1234", "admin@email.com", Role.ADMIN);
     }
 
     @AfterEach
@@ -36,52 +51,63 @@ public class ConnectionServiceTest {
 
     @Test
     void makeConnectionSuccessfulWithCandidateRoles() {
-        AppUser appUser1 = new AppUser("Joe Man", "1234", "joeman@email.com", Role.CANDIDATE);
-        AppUser appUser2 = new AppUser("Joe Woman", "1234", "joewoman@email.com", Role.CANDIDATE);
+        String returnValue = underTest.makeConnection(candidateUser1, candidateUser2);
 
-        String returnValue = connectionService.makeConnection(appUser1, appUser2);
+        ArgumentCaptor<Connection> connectionArgumentCaptor = ArgumentCaptor.forClass(Connection.class);
+        verify(connectionRepository).save(connectionArgumentCaptor.capture());
 
         assertEquals(null, returnValue);
     }
 
     @Test
     void makeConnectionSuccessfulWithCompanyRoles() {
-        AppUser appUser1 = new AppUser("Joe Man", "1234", "joeman@email.com", Role.COMPANY);
-        AppUser appUser2 = new AppUser("Joe Woman", "1234", "joewoman@email.com", Role.COMPANY);
+        String returnValue = underTest.makeConnection(companyUser, candidateUser1);
 
-        String returnValue = connectionService.makeConnection(appUser1, appUser2);
+        ArgumentCaptor<Connection> connectionArgumentCaptor = ArgumentCaptor.forClass(Connection.class);
+        verify(connectionRepository).save(connectionArgumentCaptor.capture());
 
         assertEquals(null, returnValue);
     }
 
     @Test
     void makeConnectionSuccessfulWithRecruiterRoles() {
-        AppUser appUser1 = new AppUser("Joe Man", "1234", "joeman@email.com", Role.RECRUITER);
-        AppUser appUser2 = new AppUser("Joe Woman", "1234", "joewoman@email.com", Role.RECRUITER);
+        String returnValue = underTest.makeConnection(recruiterUser, candidateUser1);
 
-        String returnValue = connectionService.makeConnection(appUser1, appUser2);
+        ArgumentCaptor<Connection> connectionArgumentCaptor = ArgumentCaptor.forClass(Connection.class);
+        verify(connectionRepository).save(connectionArgumentCaptor.capture());
 
         assertEquals(null, returnValue);
     }
 
     @Test
-    void makeConnectionWithIllegalRequesterRole() {
-        AppUser appUser1 = new AppUser("Joe Man", "1234", "joeman@email.com", Role.ADMIN);
-        AppUser appUser2 = new AppUser("Joe Woman", "1234", "joewoman@email.com", Role.CANDIDATE);
+    void makeConnectionFailWithAdminRequesterRole() {
+        ArgumentCaptor<Connection> connectionArgumentCaptor = ArgumentCaptor.forClass(Connection.class);
+        verify(connectionRepository, never()).save(connectionArgumentCaptor.capture());
 
-        Exception exception = assertThrows(IllegalStateException.class,
-                () -> connectionService.makeConnection(appUser1, appUser2),
+        assertThrows(IllegalStateException.class,
+                () -> underTest.makeConnection(adminUser, candidateUser1),
                 "Admins cannot make connections.");
     }
 
     @Test
-    void makeConnectionWithIllegalReceiverRole() {
-        AppUser appUser1 = new AppUser("Joe Man", "1234", "joeman@email.com", Role.CANDIDATE);
-        AppUser appUser2 = new AppUser("Joe Woman", "1234", "joewoman@email.com", Role.ADMIN);
+    void makeConnectionFailWithAdminReceiverRole() {
+        ArgumentCaptor<Connection> connectionArgumentCaptor = ArgumentCaptor.forClass(Connection.class);
+        verify(connectionRepository, never()).save(connectionArgumentCaptor.capture());
 
-        Exception exception = assertThrows(IllegalStateException.class,
-                () -> connectionService.makeConnection(appUser1, appUser2),
+        assertThrows(IllegalStateException.class,
+                () -> underTest.makeConnection(candidateUser1, adminUser),
                 "Admins cannot receive connections.");
+    }
+
+    @Test
+    void willThrowWhenConnectionExists() {
+        given(connectionRepository.existsById(Mockito.any(ConnectionKey.class))).willReturn(true);
+
+        verify(connectionRepository, never()).save(any());
+
+        assertThrows(IllegalStateException.class,
+                () -> underTest.makeConnection(candidateUser1, candidateUser2),
+                "Connection has already been made.");
     }
 
 }
