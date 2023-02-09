4decca56a91921d897cbb622fa9718a375021686
Chad Buck <chadbuck13@gmail.com~>
2023-02-09 10:46:39 -0500

    Fixing 1 more merge conflict

--- synapsis/src/main/java/com/soen/synapsis/appuser/registration/RegistrationController.java
+++ synapsis/src/main/java/com/soen/synapsis/appuser/registration/RegistrationController.java
@@ -2,15 +2,11 @@ package com.soen.synapsis.appuser.registration;
 
 import com.soen.synapsis.appuser.AppUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -51,17 +47,16 @@ public class RegistrationController {
             servlet.login(request.getEmail(), request.getPassword());
 
             return response;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             model.addAttribute("error", "There was an error registering you. " + e.getMessage());
             return register(model);
         }
     }
 
 
-    @GetMapping(value ="/admincreation")
+    @GetMapping(value = "/admincreation")
     public String registerAdmin(Model model) {
-        if (isUserAuthenticated()) {
+        if (AppUser.isUserAuthenticated()) {
             model.addAttribute("registrationRequest", new RegistrationRequest());
 
             return "pages/adminCreationPage";
@@ -69,23 +64,22 @@ public class RegistrationController {
         return null;
     }
 
-    @PostMapping (value ="/admincreation")
+    @PostMapping(value = "/admincreation")
     public String registerAdmin(RegistrationRequest request,
-                           BindingResult bindingResult,
-                           Model model) {
+                                BindingResult bindingResult,
+                                Model model) {
 
-        try{
+        try {
 
             if (bindingResult.hasErrors()) {
                 throw new Exception();
             }
 
-            if (isUserAuthenticated()){
+            if (AppUser.isUserAuthenticated()) {
                 String response = registrationService.registerAdmin(request);
                 return response;
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             model.addAttribute("error", "There was an error registering the ADMIN. " + e.getMessage());
             return registerAdmin(model);
         }
@@ -96,9 +90,9 @@ public class RegistrationController {
     @GetMapping("/passwordreset")
     public String passwordReset(Model model) {
 
-            model.addAttribute("registrationRequest", new RegistrationRequest());
+        model.addAttribute("registrationRequest", new RegistrationRequest());
 
-            return "pages/passwordResetForm";
+        return "pages/passwordResetForm";
 
     }
 
@@ -111,10 +105,9 @@ public class RegistrationController {
                 throw new Exception();
             }
 
-                String response = registrationService.updateUserPassword(request);
-                return response;
-        }
-        catch (Exception e) {
+            String response = registrationService.updateUserPassword(request);
+            return response;
+        } catch (Exception e) {
             model.addAttribute("error", "There was an error resetting your password. " + e.getMessage());
             return passwordReset(model);
         }
