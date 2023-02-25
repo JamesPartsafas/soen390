0445ebf5ffa1946009c45897509186b6c75deb05
Ghaith Chrit <ghaithfinadnan@gmail.com>
2023-02-25 11:46:11 -0500

    #29 Add WebSocket configurations

---
+++ synapsis/src/main/java/com/soen/synapsis/websockets/WebSocketConfig.java
@@ -0,0 +1,39 @@
+package com.soen.synapsis.websockets;
+
+import com.soen.synapsis.appuser.AuthService;
+import com.soen.synapsis.utility.ExcludeFromGeneratedTestReport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.util.Map;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    @ExcludeFromGeneratedTestReport
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/queue");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    @ExcludeFromGeneratedTestReport
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .withSockJS();
+    }
+
+}
