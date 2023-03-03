e3dd8e5137170454f5626d51307864036a45c3c0
Tony Yang <yangtony2000@gmail.com>
2023-03-02 21:22:26 -0500

    #52 changed deleteJob from GET to POST

--- synapsis/src/main/java/com/soen/synapsis/appuser/job/JobController.java
+++ synapsis/src/main/java/com/soen/synapsis/appuser/job/JobController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -96,8 +93,8 @@ public class JobController {
         }
     }
 
-    @GetMapping("/deletejob/{jid}")
-    public String deleteJob(@PathVariable Long jid, Model model) {
+    @PostMapping("/deletejob")
+    public String deleteJob(@RequestParam("jid") Long jid, Model model) {
         Optional<Job> optionalJob = jobService.getJob(jid);
 
         if (optionalJob.isEmpty() || authService.getAuthenticatedUser().getId() != optionalJob.get().getID())
