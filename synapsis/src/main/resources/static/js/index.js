61a5a045ceccfe21a24fb6652c64c715352a8be3
Jan Ong <janong24@gmail.com>
2023-03-05 11:55:54 -0500

    Minor code changes in script. May need to modify to accommodate entire site.
    #46

--- synapsis/src/main/resources/static/js/index.js
+++ synapsis/src/main/resources/static/js/index.js
@@ -1,4 +1,6 @@
 //Insert common scripts to this file
+
+//changes language upon selection
 function changeLang(langCode) {
     Array.from(document.getElementsByClassName('lang')).forEach(function (elem) {
         if (elem.classList.contains('lang' + langCode)) {
@@ -10,14 +12,16 @@ function changeLang(langCode) {
     });
 }
 
+//gets language from footer
 const selector = document.getElementById('langSelector');
 selector.addEventListener('change', function (evt) {
-    changeLanguage(this.value);
+    changeLang(this.value);
 });
 
+//set initial language to english
 const lang = navigator.language || 'en-EN';
-const startLang = Array.from(selector.options).map(opt => opt.value).find(val => lang.includes(val)) || 'en';
-changeLanguage(startLang);
+const startLang = Array.from(selector.options).map(opt => opt.value).find(val => lang.includes(val)) || 'EN';
+changeLang(startLang);
 
 selector.selectedIndex = Array.from(selector.options).map(opt => opt.value).indexOf(startLang)
 
