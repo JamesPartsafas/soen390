ac66a73eb9a31c4e97ea47d85150276947e0c891
Jan Ong <janong24@gmail.com>
2023-03-21 19:18:49 -0400

    Still currently debugging language selector

--- synapsis/src/main/resources/static/js/index.js
+++ synapsis/src/main/resources/static/js/index.js
@@ -1,46 +1,58 @@
 //Insert common scripts to this file
 
-//changes language upon selection
-function changeLang(langCode) {
-    Array.from(document.getElementsByClassName('lang')).forEach(function (elem) {
-        if (elem.classList.contains('lang' + langCode)) {
-            elem.style.display = 'initial';
-        }
-        else {
-            elem.style.display = 'none';
-        }
-    });
+//Adds event listeners for the language selector
+window.onload = function() {
+    localStorage.setItem('localLang', 'EN')
+    const selector = document.querySelector('.langSelector')
+    let currentLang = localStorage.getItem('localLang')
+    if (selector) {
+        selector.addEventListener('change', () => {
+            changeLang(currentLang)
+        })
+    }
 }
 
-//gets language from footer
-const selector = document.getElementById('langSelector');
-selector.addEventListener('change', function (evt) {
-    changeLang(this.value);
-});
-
-//set initial language to english
-const lang = navigator.language || 'en-EN';
-const startLang = Array.from(selector.options).map(opt => opt.value).find(val => lang.includes(val)) || 'EN';
-changeLang(startLang);
-
-selector.selectedIndex = Array.from(selector.options).map(opt => opt.value).indexOf(startLang)
-
-//debugger
-document.getElementById('browserLang').innerText = lang;
-document.getElementById('startLang').innerText = startLang;
-const convertBase64ToImage = () => {
-    const base64Text = document.getElementById('base64Image').innerText
-    if (base64Text == "")
-        return
-
-    let image = new Image()
-    image.src = `data:image/png;base64,${base64Text}`
-
-    let profilePictureHolder = document.getElementById('profilePictureHolder')
-    profilePictureHolder.innerHTML = null
-    profilePictureHolder.appendChild(image)
-
-    document.getElementById('base64Image').innerText = null
+//Changes the language when toggled via the language selector
+function changeLang(currentLang) {
+    const langCode = 'lang' + currentLang
+    const elements = document.getElementsByClassName(langCode)
+    if (currentLang == 'EN') {
+        const newLangCode = 'langFR'
+        const visibleElements = document.getElementsByClassName(newLangCode)
+        for (const elem of elements) {
+            elem.style.display = 'none'
+        }
+        for (const visibleElem of visibleElements) {
+            visibleElem.style.display = 'inline'
+        }
+        localStorage.setItem('localLang', 'FR')
+    }
+    else {
+        const newLangCode = 'langEN'
+        const visibleElements = document.getElementsByClassName(newLangCode)
+        for (const elem of elements) {
+            elem.style.display = 'none'
+        }
+        for (const visibleElem of visibleElements) {
+            visibleElem.style.display = 'inline'
+        }
+        localStorage.setItem('localLang', 'EN')
+    }
 }
 
-convertBase64ToImage()
+// const convertBase64ToImage = () => {
+//     const base64Text = document.getElementById('base64Image').innerText
+//     if (base64Text == "")
+//         return
+//
+//     let image = new Image()
+//     image.src = `data:image/png;base64,${base64Text}`
+//
+//     let profilePictureHolder = document.getElementById('profilePictureHolder')
+//     profilePictureHolder.innerHTML = null
+//     profilePictureHolder.appendChild(image)
+//
+//     document.getElementById('base64Image').innerText = null
+// }
+//
+// convertBase64ToImage()
