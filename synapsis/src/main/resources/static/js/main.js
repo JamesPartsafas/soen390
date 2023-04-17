8b8c8a32c6a3dca46c60aa1ec9df0c0683c47950
Jan Ong <janong24@gmail.com>
2023-04-17 19:40:07 -0400

    Fixed responsiveness of chat page

--- synapsis/src/main/resources/static/js/main.js
+++ synapsis/src/main/resources/static/js/main.js
@@ -223,27 +223,33 @@ function createMessageElement(message) {
             </div>
             ${message.id !== 0 ?
 
-        `<form action="${window.location.origin + '/chat/report'}" method="POST">
+        `<form action="${window.location.origin + '/chat/report'}" method="POST" enctype="multipart/form-data">
             <input type="hidden" name="_csrf" value="${csrfToken}"/>
             <input type="hidden" name="messageID" value="${message.id}" />
             <input type="hidden" name="chatID" value="${chatId}" />
-            <div id="popup-modal" tabIndex="-1" className="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
-                <div className="relative w-full h-full max-w-md md:h-auto">
-                    <div className="relative bg-white rounded-lg">
-                        <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="popup-modal">
-                            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            <span className="sr-only">Close modal</span>
+            <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
+                <div class="relative w-full h-full max-w-md md:h-auto">
+                    <div class="relative bg-white rounded-lg">
+                        <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" data-modal-hide="popup-modal">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <span class="sr-only">Close modal</span>
                         </button>
-                        <div className="p-6 text-center">
-                            <svg aria-hidden="true" className="mx-auto mb-4 text-gray-400 w-14 h-14 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                            <p className="mb-5 text-lg font-normal text-gray-500 ">Please note that if you choose to report a message for harassment, up to 5 previous messages from the same conversation will
+                        <div class="p-6 text-center">
+                            <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            <p class="langEN mb-5 text-lg font-normal text-gray-500 ">Please note that if you choose to report a message for harassment, up to 5 previous messages from the same conversation will
                                 be reviewed by our administrative team in order to assess the situation fully. This is to ensure that appropriate action is taken and that any patterns of behavior are identified.
                                 We take harassment very seriously and aim to create a safe and respectful environment for all of our users.
                                 By reporting a message, you are helping us to achieve this goal.</p>
-                            <button data-modal-hide="popup-modal" type="submit" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300  font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                I accept
-                            </button>
-                            <button data-modal-hide="popup-modal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">I decline</button>
+                            <p class="langFR mb-5 text-lg font-normal text-gray-500 ">Veuillez noter que si vous choisissez de signaler un message à cause d'un harcèlement, jusqu'à 5 messages précédents de la même 
+                                conversation seront examiné par notre équipe administrative afin d'évaluer pleinement la situation. Il s'agit de s'assurer que des mesures appropriées sont prises et que tous les modèles
+                                 de comportement sont identifiés. Nous prenons le harcèlement très sérieux et visons à créer un environnement sécurisé et respectueux pour tous nos utilisateurs. En signalant un message, 
+                                 vous nous aidez à atteindre notre objectif.</p>
+                            <div class="flex justify-center">
+                                <button data-modal-hide="popup-modal" type="submit" class="langEN text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300  font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">I accept</button>
+                                <button data-modal-hide="popup-modal" type="submit" class="langFR text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300  font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">J'accepte</button>
+                                <button data-modal-hide="popup-modal" type="button" class="langEN text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">I decline</button>
+                                <button data-modal-hide="popup-modal" type="button" class="langFR text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Je refuse</button>
+                            </div>
                         </div>
                     </div>
                 </div>
